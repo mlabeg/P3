8322e5f6be2151ef02998b8667be3768e8aa66d2
mlabeg <100882596+mlabeg@users.noreply.github.com>
2022-07-09 00:18:58 +0200

    Upload

--- wypozyczalnia_projekt.cpp
+++ wypozyczalnia_projekt.cpp
@@ -44,8 +44,8 @@ public:
 class Uzytkownicy {
 	vector<Uzytkownik*> uzytkownicy;
 public:
-	void dodaj() {
-		uzytkownicy.push_back(new Uzytkownik(1, "Jan", "Nowak"));
+	void dodaj(Uzytkownik* user) {
+		uzytkownicy.push_back(user);
 	}
 };
 class Wypozyczenie {
@@ -57,6 +57,12 @@ public:
 	}
 	void kto_wypozycza(Uzytkownik USER) {
 		wypozyczajacy = &USER;
+	}
+};
+class Wypozyczenia {
+	vector < pair<Wypozyczenie*, Uzytkownik*>> historia_zamwowien;
+	void dodaj(Wypozyczenie* wypozyczneie, Uzytkownik* uzytkownik) {
+		historia_zamwowien.push_back(make_pair(wypozyczneie, uzytkownik));
 	}
 };
 class Biblioteka {
@@ -71,92 +77,28 @@ public:
 			cout << id++<<". " << p->opis() << endl;
 		}
 	}
-	void wypozyczenie(Pozycja* pozycja) {
+	void wypozyczenie(Wypozyczenie* wypozyczenie) {
 		Wypozyczenie wyp1;
 		wyp1.dodaj(pozycja);
 
 	}
 };
 
-
-//klasa USER
-// 
-// 
-//class zasoby_biblioteki {
-//	vector<pair<Pozycja*,int>> zasoby;
-//public:
-//	void dodaj(Pozycja* , int ilosc) {
-//		int q = 1;
-//		for (auto& p : zasoby) {
-//			if (p.first->opis() == Pozycja->opis()) {
-//				p.second += ilosc;
-//				q = 0;
-//			}
-//		}
-//		if (q)
-//			zasoby.push_back(make_pair(pozycja, ilosc));
-//	}
-//	void pokaz() {
-//		int id = 1;
-//		cout << endl << endl << "Stan wypozyczalni: "<<endl;
-//		for (auto &p : zasoby) {
-//			cout << endl <<id++ <<": "<< p.first->opis() << ", sztuk " << p.second;
-//
-//		}
-//	}
-//	Pozycja* pobierz(int id) {
-//		--id;
-//		try {
-//			if (zasoby.at(id).second>=1) {
-//				zasoby.at(id).second -= 1;
-//				return zasoby.at(id).first;
-//			}
-//			else {
-//				cout << "Brak zasobów.";
-//			}
-//		}
-//		catch (out_of_range & e) {
-//			cout << "Nieprawidlowy numer produktu";
-//		}
-//		return nullptr;
-//	}
-//};
-//class user {
-//
-//	int id;
-//	string imie;
-//	string nazwisko;
-//public:
-//	user(string imie, string nazwisko, int id) : imie(imie), nazwisko(nazwisko), id(id) {}
-//	string daj_dane{ return "ID: "+to_string(id) +", imie: " + imie + ", nazwisko : " + nazwisko; }
-//};
-//class Wypozyczenie {
-//	user* uzytkownik;
-//	vector<Pozycja*> wypozyczenie;
-//public:
-//	Wypozyczenie(Pozycja* pozycja) :Pozycja(pozycja) {}
-//	void dodaj(Pozycja* pozycja) {
-//		wypozyczenie.push_back(new Pozycja);
-//	}
-//	void drukuj() {
-//		int nr = 1;
-//		cout << endl << endl << "Wypozyczenie";
-//		for (auto&pz : wypozyczenie) {
-//			cout << endl << pz + 1 << ": " << pz->opis();
-//		}
-//	}
-//};
-
 int main() {
 	Biblioteka publiczna;
 	publiczna.dodaj(new audiobook(2, "opowiadania"));
+	publiczna.dodaj(new audiobook(5, "bajki"));
 	publiczna.dodaj(new ksiazka(3, "Huxley", "DP"));
 	publiczna.dodaj(new film(4, "Tarantino", "PRwH"));
-	audiobook adbk(1, "basnie");
-	adbk.opis();
+
+	//audiobook adbk(1, "basnie");
+	//adbk.opis();
+
+	Uzytkownicy users;
+	users.dodaj(new Uzytkownik(1, "Jan", "Nowak"));
 
 	publiczna.pokaz_zasoby();
-	publiczna.wypozyczenie(&film(4, "Tarantino", "PRwH"));
+	//publiczna.wypozyczenie(publiczna.);
 
 	return 0;
 }
